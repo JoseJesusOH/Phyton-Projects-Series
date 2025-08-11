4bca3a15318b115842b9e3e9043fc3454c78cb40
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-11 00:40:03 -0700

    Primo Versión 2
    Proyecto que determina si un numero es primo o no, con el apoyo de un recorrido para determinar si n es divisible cierto numero de veces entonces no es considerado primo caso contrario si.

---
+++ primo2.py
@@ -0,0 +1,19 @@
+print('Este programa determina si un número entero es primo')
+print()
+
+# Lee un entero
+n = int(input('Dame un numero entero: '))
+print()
+
+# Valor maximo a probar si divide a n, sqrt(n)
+rn = int(n ** 0.5)
+
+# Para i en [2, rn]
+for i in range(2, rn+1):
+    # Si n es divisible entre i, n no es primo 
+    if not n % i:
+        print(f'{n:d} no es primo')
+        break
+else:
+    print(f'{n:d} es primo')
+
